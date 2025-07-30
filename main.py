f25aeee8b85aeb13ab3d654cecb6a9fecbab5a5f
Castro_3034_tbr <danielcastrogomezzz@gmail.com>
2025-07-30 17:29:57 +0200

    Add role management and warning commands for user moderation

--- main.py
+++ main.py
@@ -323,4 +323,204 @@ async def Hello(ctx):
     """Function to say hello (Test command)"""
     await ctx.send("Hello, I am a bot created by @castro_3034_tbr")
 
+@bot.command()
+async def set_role(ctx, member: discord.Member, role: discord.Role):
+    """Function to set a role for a user (Test command)"""
+
+    #Obtain user and server information
+    UserID = member.id
+    RoleID = role.id
+
+    #Add the role to the user
+    try:
+        #Add the role to the member
+        if role in member.roles:
+            await ctx.send(f"{member.name} already has the role {role.name}.")
+            print(f"{member.name} already has the role {role.name}.")
+            return
+        else:
+            # Add the role to the member
+            await member.add_roles(role)
+
+            #Add the role to the database
+            insert_user_role_query = """
+                INSERT IGNORE INTO user_roles (
+                    user_id,
+                    role_id
+                ) VALUES (%s, %s)
+            """
+            cursor.execute(insert_user_role_query, (UserID, RoleID))
+            conn.commit()
+
+            await ctx.send(f"Role {role.name} has been assigned to {member.name}.")
+            print(f"Role {role.name} has been assigned to {member.name}.")
+    except discord.Forbidden:
+        await ctx.send(f"I don't have permission to assign the role {role.name} to {member.name}.")
+        print(f"Permission error: Cannot assign role {role.name} to {member.name}.")
+        return
+    except Exception as e:
+        await ctx.send(f"An error occurred while assigning the role: {e}")
+        print(f"Error assigning role {role.name} to {member.name}: {e}")
+        return
+
+
+@bot.command()
+async def remove_role(ctx, member: discord.Member, role: discord.Role):
+    """Function to remove a role from a user (Test command)"""
+
+    #Obtain user and server information
+    UserName = member.name
+    UserID = member.id
+
+    #Remove the role from the user
+    try:
+        #Check if the member has the role
+        if role not in member.roles:
+            await ctx.send(f"{member.name} does not have the role {role.name}.")
+            print(f"{member.name} does not have the role {role.name}.")
+            return
+        else:
+            # Remove the role from the member
+            await member.remove_roles(role)
+
+            #Remove the role from the database
+            remove_user_role_query = """
+                DELETE FROM user_roles
+                WHERE user_id = %s AND role_id = %s
+            """
+            cursor.execute(remove_user_role_query, (UserID, role.id))
+            conn.commit()
+
+            await ctx.send(f"Role {role.name} has been removed from {member.name}.")
+            print(f"Role {role.name} has been removed from {member.name}.")
+    except discord.Forbidden:
+        await ctx.send(f"I don't have permission to remove the role {role.name} from {member.name}.")
+        print(f"Permission error: Cannot remove role {role.name} from {member.name}.")
+        return
+
+
+@bot.command()
+async def user_info(ctx, member: discord.Member = None):
+    """Function to get user information."""
+
+    #Create a emmbed with user information
+    if member is None:
+        member = ctx.author
+    embed = discord.Embed(
+        title=f"User Information for {member.name}",
+        color=discord.Color.blue()
+    )
+    embed.add_field(name="User ID", value=member.id)
+    embed.add_field(name="Username", value=member.name)
+    embed.add_field(name="Display Name", value=member.display_name)
+    embed.add_field(name="Joined At", value=member.joined_at.strftime("%a, %d %B %Y, %I:%M %p UTC") if member.joined_at else "Unknown")
+    embed.add_field(name="Status", value=str(member.status))
+    embed.add_field(name="Roles", value=", ".join([role.name for role in member.roles if role.name != "@everyone"]) or "No roles")
+
+    #Calculate the warnings for the user
+    cursor.execute("SELECT COUNT(*) FROM warnings WHERE user_id = %s", (member.id,))
+    warning_count = cursor.fetchone()[0]
+    embed.add_field(name="Warnings", value=warning_count)
+
+    embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
+    embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def add_warning(ctx, member: discord.Member, * ,reason: str):
+    """Function to add a warning to a user."""
+
+    #Check if the user has permission to add warnings
+    if not ctx.author.guild_permissions.manage_messages:
+        await ctx.send("You do not have permission to add warnings.")
+        return
+    
+    # Add the warning to the database
+    insert_warning_query = """
+        INSERT INTO warnings (user_id, reason, created_at)
+        VALUES (%s, %s, NOW())
+    """
+    cursor.execute(insert_warning_query, (member.id, reason))
+    conn.commit()
+
+    #Obtein the warning ID
+    cursor.execute("SELECT LAST_INSERT_ID()")
+    warn_ID = cursor.fetchone()[0]
+
+    #Send the embed in the log channel
+    warning_embed = discord.Embed(
+        title="Warning Added",
+        description=f"{member.name} has been warned.",
+        color=discord.Color.orange()
+    )
+    warning_embed.add_field(name="Warning ID", value=warn_ID)
+    warning_embed.add_field(name="User ID", value=member.id)
+    warning_embed.add_field(name="Reason", value=reason)
+    warning_embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
+    warning_embed.set_footer(text=f"Warning added by {ctx.author.name}", icon_url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
+
+    #Calculate the warnings for the user
+    cursor.execute("SELECT COUNT(*) FROM warnings WHERE user_id = %s", (member.id,))
+    warning_count = cursor.fetchone()[0]
+    warning_embed.add_field(name="Total Warnings", value=warning_count)
+    warning_embed.timestamp = datetime.now()
+
+    log_channel = bot.get_channel(int(STAFF_CHANNEL_ID))
+    if log_channel:
+        await log_channel.send(embed=warning_embed)
+    else:
+        print(f"Log channel with ID {STAFF_CHANNEL_ID} not found.")
+
+    await ctx.send(f"Warning added for {member.name}: {reason}")
+
+@bot.command()
+async def remove_warning(ctx, member: discord.Member, warn_ID: int, *, reason: str):
+    """Function to remove a warning from a user."""
+
+    #Check if the warning exists
+    cursor.execute("SELECT COUNT(*) FROM warnings WHERE user_id = %s AND warning_id = %s", (member.id, warn_ID))
+    if cursor.fetchone()[0] == 0:
+        await ctx.send(f"No warning found for {member.name} with ID {warn_ID}.")
+        return
+
+    #Check if the user has permission to remove warnings
+    if not ctx.author.guild_permissions.manage_messages:
+        await ctx.send("You do not have permission to remove warnings.")
+        return
+
+
+    # Remove the warning from the database
+    remove_warning_query = """
+        DELETE FROM warnings
+        WHERE user_id = %s AND warning_id = %s
+    """
+    cursor.execute(remove_warning_query, (member.id, warn_ID))
+    conn.commit()
+
+    #Send the embed in the log channel
+    warning_embed = discord.Embed(
+        title="Warning Removed",
+        description=f"{member.name}'s warning has been removed.",
+        color=discord.Color.green()
+    )
+    warning_embed.add_field(name="User ID", value=member.id)
+    warning_embed.add_field(name="Reason", value=reason)
+    warning_embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
+    warning_embed.set_footer(text=f"Warning removed by {ctx.author.name}", icon_url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
+
+    #Calculate the warnings for the user
+    cursor.execute("SELECT COUNT(*) FROM warnings WHERE user_id = %s", (member.id,))
+    warning_count = cursor.fetchone()[0]
+    warning_embed.add_field(name="Total Warnings", value=warning_count)
+    warning_embed.timestamp = datetime.now()
+    log_channel = bot.get_channel(int(STAFF_CHANNEL_ID))
+    if log_channel:
+        await log_channel.send(embed=warning_embed)
+    else:
+        print(f"Log channel with ID {STAFF_CHANNEL_ID} not found.")
+
+    await ctx.send(f"Warning removed for {member.name}: {reason}")
+
+
 bot.run(TOKEN)
